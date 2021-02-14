4941bedd84d7d8a40f108f6d25a56e6a58612282
Yuriy <levin-algos@yandex.ru>
2021-02-14 14:50:05 +0300

    Junit 5 & log4j2

--- src/test/java/com/iyuriy/pipeline/LibraryTest.java
+++ src/test/java/com/iyuriy/pipeline/LibraryTest.java
@@ -1,8 +1,8 @@
 package com.iyuriy.pipeline;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LibraryTest {
     @Test
