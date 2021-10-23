d3cb4c2a159a1ec56c3e959fefc0a11ab5b7160b
Overflow922 <genius922@yandex.ru>
2021-10-23 21:32:39 +0300

    Java 17 migration

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,16 @@ plugins {
     jacoco
 }
 
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
 repositories {
     // Use jcenter for resolving your dependencies.
     // You can declare any Maven/Ivy/file repository here.
-    jcenter()
     mavenCentral()
 }
 
@@ -19,16 +25,17 @@ dependencies {
     // This dependency is exported to consumers, that is to say found on their compile classpath.
     api("org.apache.commons:commons-math3:3.6.1")
 
-    api("org.apache.logging.log4j:log4j-api:2.14.0")
-    api("org.apache.logging.log4j:log4j-core:2.14.0")
-    api("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.1")
-    api("com.fasterxml.jackson.core:jackson-databind:2.12.1")
+    api("org.apache.logging.log4j:log4j-api:2.14.1")
+    api("org.apache.logging.log4j:log4j-core:2.14.1")
+    api("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.0")
+    api("com.fasterxml.jackson.core:jackson-databind:2.13.0")
 
     // This dependency is used internally, and not exposed to consumers on their own compile classpath.
     implementation("com.google.guava:guava:31.0.1-jre")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.assertj:assertj-core:3.21.0")
 }
 
 buildscript {
@@ -62,15 +69,15 @@ tasks.jacocoTestReport {
 
 
 jacoco {
-    toolVersion = "0.8.6"
+    toolVersion = "0.8.7"
     reportsDirectory.set(file("$buildDir/customJacocoReportDir"))
 }
 
 tasks.jacocoTestReport {
     reports {
-        xml.isEnabled = false
-        csv.isEnabled = false
-        html.destination = file("${buildDir}/jacocoHtml")
+        xml.required.set(false)
+        csv.required.set(false)
+        html.outputLocation.set(file("${buildDir}/jacocoHtml"))
     }
 }
 
