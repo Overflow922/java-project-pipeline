49760de10190fb7dbb533561056d91346e50f8c9
Overflow922 <genius922@yandex.ru>
2021-10-23 21:59:48 +0300

    Update spotbugs

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 plugins {
     // Apply the java-library plugin to add support for Java Library
     `java-library`
-    id("com.github.spotbugs") version "4.6.0"
+    id("com.github.spotbugs") version "4.7.9"
     jacoco
 }
 
@@ -19,19 +19,16 @@ repositories {
     // Use jcenter for resolving your dependencies.
     // You can declare any Maven/Ivy/file repository here.
     mavenCentral()
+    maven {
+        url = uri("https://plugins.gradle.org/m2/")
+    }
 }
 
 dependencies {
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api("org.apache.commons:commons-math3:3.6.1")
-
     api("org.apache.logging.log4j:log4j-api:2.14.1")
     api("org.apache.logging.log4j:log4j-core:2.14.1")
     api("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.0")
     api("com.fasterxml.jackson.core:jackson-databind:2.13.0")
-
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:31.0.1-jre")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.8.1")
@@ -45,7 +42,7 @@ buildscript {
         }
     }
     dependencies {
-        classpath("gradle.plugin.com.github.spotbugs.snom:spotbugs-gradle-plugin:4.6.0")
+        classpath("com.github.spotbugs.snom:spotbugs-gradle-plugin:4.7.9")
     }
 }
 
